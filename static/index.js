f5e96f0244e961a07f60799ee22375cf71931efd
Zuzanna Dudzinska <zuzannadudzinska96@gmail.com>
2022-03-09 19:37:14 +0100

    show channels

--- static/index.js
+++ static/index.js
@@ -1,11 +1,41 @@
 async function getChannels() {
   try {
     const resp = await axios.get("/channels");
-    console.log(resp.data);
+    cerateChannel(resp.data);
   } catch (error) {
     console.log(error);
   }
 }
 
+function cerateChannel(channels) {
+  const elements = document.getElementById("channels");
+  const element = document.getElementById("channel-0");
+
+  if (channels.length > 0) {
+    element.classList.remove("hidden");
+    changeDataChanel(element, channels.shift(), 0);
+  } else {
+    !element.classList.contains("hidden") && element.classList.add("hidden");
+  }
+
+  channels.forEach((channel, index) => {
+    const clone = element.cloneNode(true);
+    changeDataChanel(clone, channel, index + 1);
+    elements.appendChild(clone);
+  });
+}
+
+function changeDataChanel(el, channel, id) {
+  el.id = `channel-${id}`;
+  el.getElementsByClassName("channel__name")[0].innerHTML = channel.title;
+  el.getElementsByClassName("subscribers")[0].innerHTML =
+    channel.statistics.subscriberCount;
+  el.getElementsByClassName("videos")[0].innerHTML =
+    channel.statistics.videoCount;
+  el.getElementsByClassName("views")[0].innerHTML =
+    channel.statistics.viewCount;
+  el.getElementsByClassName("channel__image")[0].src =
+    channel.thumbnails.high.url;
+}
 
 window.addEventListener("load", getChannels);
